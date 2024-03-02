d4085ca93941c105a4ac69e94aae95a8d2f5b827
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-02 19:21:05 +1030

    sd

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <algorithm>
 
-int median_array(int array[], int n){
-    const int size = n;
-    std::sort(array, array + size);
-
-    return array[n/2];
+int median_array(int array[], int n) {
+    if (n % 2 == 0 || n < 1) {
+        return 0; 
+    } else {
+        const int size = n;
+        std::sort(array, array + size);
+        return array[n / 2];
+    }
 }
