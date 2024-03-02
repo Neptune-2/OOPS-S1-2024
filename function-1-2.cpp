a20f5a7319aca2e165a124d054008f5672f3c303
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-02 17:05:10 +1030

    ss

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
-double array_mean(int array[], int n){
+double array_mean(int array[], int n) {
     int sum = 0;
     double mean = 0;
-    if (n > 0){
-        for(int i=0; i<n; i++){
+
+    if (n > 0) {
+        for (int i = 0; i < n; i++) {
             sum += array[i];
         }
-        mean = sum/n;
-        } else {
-            mean = 0.0;
-        }
+        mean = static_cast<double>(sum) / n;
+    } else {
+        mean = 0.0;
+    }
+
     return mean;
 }
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include "function-1-2.h"
 
 int main() {
     int myArray[] = {1, 2, 3, 4, 5};
     double mean = array_mean(myArray, 5);
-    std::cout << "Mean: " << mean << std::endl;
+    std::cout << std::fixed << std::setprecision(1) << mean << std::endl;
     return 0;
 }
