02f668f2bcfb3a07211a4d5531aeddc20804b6d8
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-02 19:44:18 +1030

    jkjk

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
+#include <unordered_map>
 
-double weighted_average(int array[], int n){
-    if(n<1){
+double weighted_average(int array[], int n) {
+    if (n < 1) {
         return 0.0;
     }
 
-    int ones=0;
-    int twos=0;
-    int threes=0;
-    int fours=0;
-    int fives=0;
-    int sixes=0;
-    int sevens=0;
-    int eights=0;
-    int nines=0;
+    std::unordered_map<int, int> elementCounts;
 
-    for(int i=0; i<n; i++){
-        if(array[i]==2){
-            twos++;
-        } else if(array[i]==5){
-            fives++;
-        } else if(array[i]==9){
-            nines++;
-        } else if (array[i]==1){
-            ones++;
-        } else if (array[i]==3){
-            threes++;
-        } else if (array[i]==4){
-            fours++;
-        } else if (array[i]==6){
-            sixes++;
-        } else if (array[i]==7){
-            sevens++;
-        } else if (array[i]==8){
-            eights++;
-        }
+    for (int i = 0; i < n; i++) {
+        elementCounts[array[i]]++;
     }
 
-    double sum = (1*ones)/n + (2*twos)/n + (3*threes)/n + (4*fours)/n + (5*fives)/n + (6*sixes)/n +(7*sevens)/n + (8*eights)/n + (9*nines)/n;
-    return sum/n;
+    double sum = 0.0;
+
+    for (const auto& entry : elementCounts) {
+        int xi = entry.first;
+        int count = entry.second;
+        sum += static_cast<double>(xi * count) / n;
+    }
+
+    return sum;
 }
