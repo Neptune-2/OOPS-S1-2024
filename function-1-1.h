c7ed02803746c98621f35a74d28f368e46dccea6
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-02 16:38:54 +1030

    kk

---
+++ function-1-1.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION11_H  // Include guard
+#define FUNCTION11_H
+
+int array_sum(int array[], int n);
+
+#endif // FUNCTION11_H
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,3 +1,9 @@
-#include <
+#include <iostream>
+#include "function-1-1.h"
 
-int array_sum(int array[], int n)
+int main() {
+    int myArray[] = {1, 2, 3, 4, 5};
+    int sum = array_sum(myArray, 5);
+    std::cout << "Sum: " << sum << std::endl;
+    return 0;
+}
