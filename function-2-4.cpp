896c1bdd69b3362396095452fd26007c91b7b3f1
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-02 18:27:33 +1030

    ss

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -2,7 +2,7 @@
 
 bool is_ascending(int array[], int n){
     bool truth = 1;
-    for(int i=0; i<n; i++){
+    for(int i=1; i<n; i++){
         if(array[i]<array[i-1]){
             truth = 0;
         }
