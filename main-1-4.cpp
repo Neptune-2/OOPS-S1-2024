657cbd651b2255c47912ce2753d134d5c00691e2
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-22 20:41:14 +1030

    jkjk

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -2,7 +2,17 @@
 #include <stdio.h>
 #include <iostream>
 
+PersonList createPersonList(int n){
+    Person* array = createPersonArray1(n);
+    PersonList finalArray;
+    finalArray.numPeople = n;
+    finalArray.people = array;
+    
+    return finalArray;
+}
+
 int main(){
     PersonList array = createPersonList(2);
-    PersonList array2 = shallowCopyPersonList(array);
+    PersonList array2;
+    array2 = shallowCopyPersonList(array);
 }
