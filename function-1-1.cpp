430bf2cc344382848109b9efe89719a0c1df177b
Neptune-2 <a1898256@adelaide.edu.au>
2024-03-22 18:33:50 +1030

    s

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -9,11 +9,3 @@ Person* createPersonArray(int n) {
     }
     return array;
 }
-
-Person* createPersonArray1(int n) {
-    Person* array = new Person[n];
-    for(int i=0; i<n; i++){
-        array[i] = {"Jane Doe", 1};
-    }
-    return array;
-}
